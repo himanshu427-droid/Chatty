56be77e63ab65952ffba80fbde549431afb9cac7
Himanshu Soni <himanshusonimtr@gmail.com>
2025-03-20 20:59:51 +0530

    Updated Frontend structure

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const NoChatSelected = () => {
   return (
-    <div>NoChatSelected</div>
+  <div className='w-full h-full'>
+   <div className="mockup-window flex flex-1 bg-base-100 border border-base-300 w-full h-full">
+  <div className="grid place-content-center w-full h-full">
+  </div>
+  </div>
+    </div>
   )
 }
 
